992123b47f5dd7aaf2da58790cf3f922993b25a0
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-07-27 08:13:14 +0100

    Commit

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="copyright-wrapper">
     <div class="copyright-area">
-        <p>Developed by <a href="https://appdevs.net">AppDevs.</a></p>
+        <p>Developé par <a href="#t">JOBS CONSEIL.</a></p>
         <p>{{ $basic_settings->site_name }} <span>V1.0.0</span></p>
     </div>
 </div>

5983d72b01bea301453628d8baf62b6ec42a0ab8
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-08-19 08:36:29 +0100

    dbg 1

--- resources/views/user/partials/preloader.blade.php
+++ resources/views/user/partials/preloader.blade.php
@@ -1,8 +1,7 @@
 <div class="preloader">
     <div class="loader-inner">
         <div class="loader-circle">
-            <img src="{{ asset('public/backend/') }}images/web-settings/image-assets/94a9e3a5-7b10-4411-8e40-b6b742419981.webp"
-                alt="Preloader" />
+            <img src="{{ asset('public/frontend/') }}/images/logo/fav.png" alt="Preloader" />
         </div>
         <div class="loader-line-mask">
             <div class="loader-line"></div>

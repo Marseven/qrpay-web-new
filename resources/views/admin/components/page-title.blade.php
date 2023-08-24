f51298a168cf242a9dabc2895571917f1dc0f3f2
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-08-24 14:33:05 +0100

    dbg 6

--- resources/views/admin/components/page-title.blade.php
+++ resources/views/admin/components/page-title.blade.php
@@ -6,7 +6,7 @@
     </div>
     <div class="content">
         <h3 class="title">{{ $title }}</h3>
-        <p>{{ isset($sub_title) ? $sub_title : "Bienvu dans l'espace d'administration de " . $basic_settings->site_name . ' ' }}
+        <p>{{ isset($sub_title) ? $sub_title : "Bienvenu dans l'espace d'administration de " . $basic_settings->site_name . ' ' }}
         </p>
     </div>
 </div>

a6b96ad089b523c66176ba5ec06c09dcea17a035
waly2020 <walyguema@gmail.com>
2023-09-25 22:33:26 +0100

    Ajout de la page menu

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -14,7 +14,7 @@ class MenuController extends Controller
         $menu = new Menu();
         $plats = new Plats();
         $menus = Menu::find(1);
-        dd($menus->plats[0]);
+        dd($menus->plats[0]->titre);
         return view("user.sections.menu.index",["menus" => $menu->all(),"plats" => $plats->all()]);
     }
     public function create(Request $req){

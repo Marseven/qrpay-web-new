23d6d7fa13fd643d8aab2f5542a0b2076a3da3cc
waly2020 <walyguema@gmail.com>
2023-09-27 05:05:13 +0100

    Ajout de la page menu

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -26,6 +26,6 @@ class MenuController extends Controller
         return $menu->all();
     }
     function apiGetMenu() {
-        return Menu::all();
+        return ["menu" => Menu::all(),"plats" => Plats::all()];
     }
 }
